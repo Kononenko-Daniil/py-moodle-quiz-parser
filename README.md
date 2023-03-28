532e483cb0bc5168892c6be584ef26655a5f619f
Kononenko-Daniil <neokv210@gmail.com>
2023-03-28 20:30:53 +0300

    Setup changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # py-moodle-quiz-parser
+
 This is a repo with Python package for parsing Moodle quizes
--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup
 
 from MoodleQuizParser import __version__
 
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+
 setup(
     version=__version__,
     name='py-moodle-quiz-parser',
     description='PIP package for parsing moodle quiz HTML documents',
-    description_content_type='text/x-rst',
-    long_description=open('README.txt').read(),
+    long_description=readme,
     long_description_content_type='text/markdown',
     url='https://github.com/Kononenko-Daniil/py-moodle-quiz-parser',
     author='Daniil Kononenko',
