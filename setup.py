4e8b72bdbe4d5c376b77defdadf1bd893218b95e
Kononenko-Daniil <neokv210@gmail.com>
2023-03-28 11:53:51 +0300

    Package added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+from MoodleQuizParser import __version__
+
+setup(
+    version=__version__,
+    name='py-moodle-quiz-parser',
+    description='PIP package for parsing moodle quiz HTML documents',
+    url='https://github.com/Kononenko-Daniil/py-moodle-quiz-parser',
+    author='Daniil Kononenko',
+    packages=['MoodleQuizParser'],
+    install_requires=[
+        'beautifulsoup4'
+    ]
+)
