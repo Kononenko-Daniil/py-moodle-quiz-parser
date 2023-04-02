54eb6cdda829170dcfe207c66a6f571158a93911
Kononenko-Daniil <neokv210@gmail.com>
2023-04-02 15:37:21 +0300

    Fix bug with argument in Question.__init__()

--- MoodleQuizParser/Models/Question.py
+++ MoodleQuizParser/Models/Question.py
@@ -1,14 +1,17 @@
+from Answer import Answer
+
 DEFAULT_QUESTION_TEXT = "--question--"
 DEFAULT_MAX_GRADE = 1.0
 DEFAULT_GRADE = 0.0
+DEFAULT_ANSWER = "--answer--"
 
 
 class Question:
     def __init__(self,
-                 answer,
                  text=DEFAULT_QUESTION_TEXT,
                  max_grade=DEFAULT_MAX_GRADE,
-                 grade=DEFAULT_GRADE):
+                 grade=DEFAULT_GRADE,
+                 answer=DEFAULT_ANSWER):
         self.text = text
         self.max_grade = max_grade
         self.grade = grade
@@ -19,7 +22,7 @@ class Question:
             "question": self.text,
             "max_grade": self.max_grade,
             "grade": self.grade,
-            "answer": self.answer.to_dict()
+            "answer": self.answer if self.answer == DEFAULT_ANSWER else self.answer.to_dict()
         }
 
         return dict_obj
